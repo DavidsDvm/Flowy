19a099f62602117464182fcde331949b4d8ffe51
DavidsDvm <davids10.dvm@gmail.com>
2021-05-27 01:02:08 -0500

    trying to solve img loading problems on the index

--- help/js/imports_jQuery.js
+++ help/js/imports_jQuery.js
@@ -10,12 +10,12 @@ if (window.location.pathname == "/Flowy/index.html"){
 
 $( document ).ready(function() {
     if (window.location.pathname == "/Flowy/"){
-        document.getElementById('header_logo').src = 'help/img/Logo_Flowy.svg';
-        document.getElementById('header_shopping_cart').src = 'help/img/shopping_cart_1.svg';
-        document.getElementById('footer_copyright_symbol').src = 'help/img/cophyright_symbol.svg';
-        document.getElementById('footer_ubication_symbol').src = 'help/img/ubication_symbol.svg';
-        document.getElementById('footer_at_symbol').src = 'help/img/at_sing.svg';
-        document.getElementById('footer_up_arrow_symbol').src = 'help/img/up_arrow.svg';
+        document.getElementById('header_logo').src = 'Flowy/help/img/Logo_Flowy.svg';
+        document.getElementById('header_shopping_cart').src = 'Flowy/help/img/shopping_cart_1.svg';
+        document.getElementById('footer_copyright_symbol').src = 'Flowy/help/img/cophyright_symbol.svg';
+        document.getElementById('footer_ubication_symbol').src = 'Flowy/help/img/ubication_symbol.svg';
+        document.getElementById('footer_at_symbol').src = 'Flowy/help/img/at_sing.svg';
+        document.getElementById('footer_up_arrow_symbol').src = 'Flowy/help/img/up_arrow.svg';
     }
   });
 
