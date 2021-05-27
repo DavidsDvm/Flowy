c71fe156dc4c63eb503662c7d9bafc9a9c16780b
DavidsDvm <davids10.dvm@gmail.com>
2021-05-27 02:31:23 -0500

    changed the 'document' to '#header_shopping_cart'

--- help/js/imports_jQuery.js
+++ help/js/imports_jQuery.js
@@ -8,7 +8,7 @@ if (window.location.pathname == "/Flowy/index.html"){
     window.location.href = "../Flowy";
 }
 
-$(document).ready(function() {
+$("#header_shopping_cart").ready(function() {
     sessionStorage.setItem("numStopLoop", 0);
 
     if (window.location.pathname == "/Flowy/" && sessionStorage.getItem("numStopLoop")  == 0){
