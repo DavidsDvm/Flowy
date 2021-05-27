410eadaa0c8b0b650cee797fac025b1622e1a2e5
DavidsDvm <davids10.dvm@gmail.com>
2021-05-27 02:21:01 -0500

    probably solved the problem

--- help/js/imports_jQuery.js
+++ help/js/imports_jQuery.js
@@ -8,6 +8,20 @@ if (window.location.pathname == "/Flowy/index.html"){
     window.location.href = "../Flowy";
 }
 
+$( document ).ready(function() {
+    sessionStorage.setItem("numStopLoop", 0);
+
+    if (window.location.pathname == "/Flowy/" && sessionStorage.getItem("numStopLoop")  == 0){
+        document.getElementById('header_logo').src = 'Flowy/help/img/Logo_Flowy.svg';
+        document.getElementById('header_shopping_cart').src = 'Flowy/help/img/shopping_cart_1.svg';
+        document.getElementById('footer_copyright_symbol').src = 'Flowy/help/img/cophyright_symbol.svg';
+        document.getElementById('footer_ubication_symbol').src = 'Flowy/help/img/ubication_symbol.svg';
+        document.getElementById('footer_at_symbol').src = 'Flowy/help/img/at_sing.svg';
+        document.getElementById('footer_up_arrow_symbol').src = 'Flowy/help/img/up_arrow.svg';
+        sessionStorage.setItem("numStopLoop", 1);
+    }
+});
+
 function myUrlFun(site){
     var actualUrl = window.location.pathname.split('/');
     console.log(actualUrl);
