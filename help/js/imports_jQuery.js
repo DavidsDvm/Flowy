cd7a856a2ab7a3078fd25b9a7e9a8380c884a64a
DavidsDvm <davids10.dvm@gmail.com>
2021-05-27 02:48:31 -0500

    Creation of a new JS file and deletion of some lines on the jQuery JS

--- help/js/imports_jQuery.js
+++ help/js/imports_jQuery.js
@@ -10,20 +10,6 @@ $( document ).ready(function() {
     }
 });
 
-$("#header_shopping_cart").ready(function() {
-    sessionStorage.setItem("numStopLoop", 0);
-
-    if (window.location.pathname == "/Flowy/" && sessionStorage.getItem("numStopLoop")  == 0){
-        document.getElementById('header_logo').src = 'help/img/Logo_Flowy.svg';
-        document.getElementById('header_shopping_cart').src = 'help/img/shopping_cart_1.svg';
-        document.getElementById('footer_copyright_symbol').src = 'help/img/cophyright_symbol.svg';
-        document.getElementById('footer_ubication_symbol').src = 'help/img/ubication_symbol.svg';
-        document.getElementById('footer_at_symbol').src = 'help/img/at_sing.svg';
-        document.getElementById('footer_up_arrow_symbol').src = 'help/img/up_arrow.svg';
-        sessionStorage.setItem("numStopLoop", 1);
-    }
-});
-
 function myUrlFun(site){
     var actualUrl = window.location.pathname.split('/');
     console.log(actualUrl);
