e989446ed2f51679ce98716647b387345733a957
DavidsDvm <davids10.dvm@gmail.com>
2021-06-22 16:14:36 -0500

    Solved some problems on the final test

--- help/js/imports_jQuery.js
+++ help/js/imports_jQuery.js
@@ -11,15 +11,15 @@ $( "#footer__main--jQuery" ).load( "../view/footer.html", function(){
 });
 
 $( "#footer__index--jQuery" ).load( "view/index_footer.html", function(){
-    document.getElementById('header_logo').src = 'help/img/Logo_Flowy.svg';
-    document.getElementById('header_shopping_cart').src = 'help/img/shopping_cart_1.svg';
-});
-
-$( "#header__navbar--index-jQuery" ).load( "view/header.html", function(){
     document.getElementById('footer_copyright_symbol').src = 'help/img/cophyright_symbol.svg';
     document.getElementById('footer_ubication_symbol').src = 'help/img/ubication_symbol.svg';
     document.getElementById('footer_at_symbol').src = 'help/img/at_sing.svg';
     document.getElementById('footer_up_arrow_symbol').src = 'help/img/up_arrow.svg';
+});
+
+$( "#header__navbar--index-jQuery" ).load( "view/header.html", function(){
+    document.getElementById('header_logo').src = 'help/img/Logo_Flowy.svg';
+    document.getElementById('header_shopping_cart').src = 'help/img/shopping_cart_1.svg';
 });
 
 // Si entra a index.html se le llevara a flowy normal
