6f8cf6d5f13e9e1a31486f73bb5df0a7f594c8de
DavidsDvm <davids10.dvm@gmail.com>
2021-06-22 16:10:21 -0500

    Problem totally solved

--- help/js/imports_jQuery.js
+++ help/js/imports_jQuery.js
@@ -1,9 +1,20 @@
-$( "#header__navbar--jQuery" ).load( "../view/header.html" );
-$( "#footer__main--jQuery" ).load( "../view/footer.html" );
+$( "#header__navbar--jQuery" ).load( "../view/header.html", function(){
+    document.getElementById('header_logo').src = '../help/img/Logo_Flowy.svg';
+    document.getElementById('header_shopping_cart').src = '../help/img/shopping_cart_1.svg';
+});
+
+$( "#footer__main--jQuery" ).load( "../view/footer.html", function(){
+    document.getElementById('footer_copyright_symbol').src = '../help/img/cophyright_symbol.svg';
+    document.getElementById('footer_ubication_symbol').src = '../help/img/ubication_symbol.svg';
+    document.getElementById('footer_at_symbol').src = '../help/img/at_sing.svg';
+    document.getElementById('footer_up_arrow_symbol').src = '../help/img/up_arrow.svg';
+});
+
 $( "#footer__index--jQuery" ).load( "view/index_footer.html", function(){
     document.getElementById('header_logo').src = 'help/img/Logo_Flowy.svg';
     document.getElementById('header_shopping_cart').src = 'help/img/shopping_cart_1.svg';
 });
+
 $( "#header__navbar--index-jQuery" ).load( "view/header.html", function(){
     document.getElementById('footer_copyright_symbol').src = 'help/img/cophyright_symbol.svg';
     document.getElementById('footer_ubication_symbol').src = 'help/img/ubication_symbol.svg';
