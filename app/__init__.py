097230d567d06f6b45433a10ce4d53e5745604fe
DavidsDvm <davids10.dvm@gmail.com>
2021-08-04 09:00:02 -0500

    modification of the app init for the db conection

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,8 @@ from flask import Flask
 
 from .config import Config
 from .auth import auth
+from .panel import panel
+from .models import db
 
 def create_app():
     app = Flask(__name__)
@@ -9,5 +11,7 @@ def create_app():
     app.config.from_object(Config)
 
     app.register_blueprint(auth)
+    app.register_blueprint(panel)
 
+    db.init_app(app)
     return app
