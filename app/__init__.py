160a92af5ad406e15e9852b24c7e2831ff27fa70
DavidsDvm <davids10.dvm@gmail.com>
2021-08-11 09:36:07 -0500

    Modification of the app init file adding login forms and other things

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,12 @@
 from flask import Flask
 
+
 from .config import Config
 from .auth import auth
+from .auth.views import login_manager
 from .panel import panel
 from .models import db
+from .forms import csrf
 
 def create_app():
     app = Flask(__name__)
@@ -14,4 +17,6 @@ def create_app():
     app.register_blueprint(panel)
 
     db.init_app(app)
+    login_manager.init_app(app)
+    csrf.init_app(app)
     return app
