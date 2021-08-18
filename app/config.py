d2b0b485d2ed1e6793f1788da876f79109cd06f2
DavidsDvm <davids10.dvm@gmail.com>
2021-08-18 12:19:19 -0500

    Modification of the configs

--- app/config.py
+++ app/config.py
@@ -1,4 +1,4 @@
 class Config:
     SECRET_KEY = 'SUPER SECRET'
-    SQLALCHEMY_DATABASE_URI = 'mariadb+mariadbconnector://root:''@127.0.0.1:3306/flowysi'
+    SQLALCHEMY_DATABASE_URI = 'mariadb+mariadbconnector://root:''@127.0.0.1:3306/FlowySI'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
