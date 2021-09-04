ab63a1cf518290b674bb19a1bf95abbb278aef66
DavidsDvm <davids10.dvm@gmail.com>
2021-09-04 14:56:30 -0500

    added the function of see the products on the 'compras' query and 'pedidos' query

--- app/panel/views.py
+++ app/panel/views.py
@@ -1,28 +1,25 @@
 from flask import render_template, session, redirect, url_for, request
+from sqlalchemy import Integer, func
 from datetime import date
 
 from . import panel
-from ..models import pedido, compra, db
+from ..models import pedido, compra, pedidoProducto, producto, empleado, proovedor,db 
 
 @panel.route('/', methods=['GET', 'POST'])
 @panel.route('/inicio', methods=['GET', 'POST'])
 def inicioPanel():
+    ultimasVentas = db.session.query(pedido).order_by(pedido.idPedido.desc()).limit(5)
+    ultimasVentasSuma = pedido.query.with_entities(func.sum(pedido.totalPedido).label("mySum")).first()
+    ultimosRegistrosSuma = db.session.query(pedido).count() 
+
     context = {
-        'personalName': session['username']
+        'personalName' : session['username'],
+        'ultimasVentas' : ultimasVentas,
+        'ultimasGanancias' : int(ultimasVentasSuma.mySum),
+        'totalRegistros'  : ultimosRegistrosSuma
     }
 
     return render_template('panelIndex.html', **context)
-
-@panel.route('/compras')
-def comprasPanel():
-    all_data = compra.query.all()
-
-    context = {
-        'personalName': session['username'],
-        'compras' : all_data
-    }
-
-    return render_template('panelCompras.html', **context)
 
 # @panel.route('/usuarios', defaults={'_route': 'panelUsuarios'})
 # def usuariosPanel():
@@ -41,7 +38,21 @@ def perfilPanel():
 
     return render_template('panelPerfil.html', **context)
 
+@panel.route('/compras')
+def comprasPanel():
+    all_data = compra.query.filter(compra.estadoCompra != 'Inactivo').all()
+    compras = {}
+    for key, data in enumerate(all_data, start=1):
+        empleadoNombre = empleado.query.filter_by(idEmpleado = data.idEmpleado).first().nombreEmpleado
+        nombreProvedor = proovedor.query.filter_by(idProovedor = data.idProovedor).first().nombreProovedor
+        compras[key] = {'especificacionCompra' : data.especificacionCompra, 'totalCompra' : data.totalCompra, 'empleadoNombre' : empleadoNombre, 'fechaCompra' : data.fechaCompra, 'proovedorNombre' : nombreProvedor, 'idCompras' : data.idCompra, 'idProovedor' : data.idProovedor}
 
+    context = {
+        'personalName': session['username'],
+        'compras' : compras
+    }
+
+    return render_template('panelCompras.html', **context)
 
 @panel.route('/compras/insert', methods =['POST'])
 def addCompras():
@@ -76,6 +87,7 @@ def editCompras(id):
 @panel.route('/compras/delete/<int:id>', methods = ['GET', 'POST'])
 def deleteCompras(id):
     my_data = compra.query.get(id)
+    my_data.estadoCompra = "Inactivo"
 
     db.session.delete(my_data)
     db.session.commit()
@@ -84,11 +96,29 @@ def deleteCompras(id):
 
 @panel.route('/pedidos')
 def pedidosPanel():
-    all_data = pedido.query.all()
+    all_data = pedido.query.filter(pedido.estadoPedido != 'Inactivo').all()
 
     context = {
         'personalName': session['username'],
         'pedidos' : all_data
+    }
+
+    return render_template('panelPedidos.html', **context)
+
+@panel.route('/pedidos/<int:id>')
+def pedidosPanelEspecific(id):
+    all_data = pedido.query.all()
+    my_data = pedidoProducto.query.filter_by(idPedido = id)
+    productos = {}
+    for key, data in enumerate(my_data, start=1):
+        productoNombre = producto.query.filter_by(idProducto = data.idProducto).first().nombreProducto
+        productos[key] = {'nombre' : productoNombre, 'subtotal' : data.subtotalPedidoProducto, 'cantidad' : data.cantidadPedidoProducto}
+    print(productos)
+
+    context = {
+        'personalName': session['username'],
+        'pedidos' : all_data,
+        'productos' : productos
     }
 
     return render_template('panelPedidos.html', **context)
@@ -129,7 +159,7 @@ def editPedidos(id):
 def deletePedidos(id):
     my_data = pedido.query.get(id)
 
-    db.session.delete(my_data)
+    my_data.estadoPedido = "Inactivo"
     db.session.commit()
 
     return redirect(url_for('panel.pedidosPanel'))
